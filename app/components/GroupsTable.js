a9ca52cfccded6d08765b6a3e22a591eb571a8f6
chemicstry <chemicstry@gmail.com>
2019-02-22 00:54:03 +0200

    Add table filtering and sorting

--- app/components/GroupsTable.js
+++ app/components/GroupsTable.js
@@ -1,22 +1,29 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import * as rpc from '../utils/rpc';
-import { Table, Popover, Tag, Button, message } from 'antd';
+import { Table, Popover, Tag, Button, Input, Icon, message } from 'antd';
+import Highlighter from 'react-highlight-words';
 
 const { Column } = Table;
 
 export default class GroupsTable extends Component {
   state = {
-    groups: []
+    groups: [],
+    permissions: [] // list of all permissions for filtering
   }
 
   async fetchGroups() {
     var groups = await rpc.client.call('admin:getGroups');
+    var permissions = [];
     groups.forEach((group) => {
       group.key = group.id;
+      group.permissions.forEach(permission => {
+        if (!permissions.includes(permission))
+          permissions.push(permission);
+      })
     });
     console.log(groups);
-    this.setState({groups})
+    this.setState({groups, permissions})
   }
 
   componentDidMount() {
@@ -51,24 +58,90 @@ export default class GroupsTable extends Component {
     return <div>{shown}{hidden}</div>;
   }
 
+  getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({
+      setSelectedKeys, selectedKeys, confirm, clearFilters,
+    }) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          ref={node => { this.searchInput = node; }}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => this.handleSearch(selectedKeys, confirm)}
+          style={{ width: 188, marginBottom: 8, display: 'block' }}
+        />
+        <Button
+          type="primary"
+          onClick={() => this.handleSearch(selectedKeys, confirm)}
+          icon="search"
+          size="small"
+          style={{ width: 90, marginRight: 8 }}
+        >
+          Search
+        </Button>
+        <Button
+          onClick={() => this.handleReset(clearFilters)}
+          size="small"
+          style={{ width: 90 }}
+        >
+          Reset
+        </Button>
+      </div>
+    ),
+    filterIcon: filtered => <Icon type="search" style={{ color: filtered ? '#1890ff' : undefined }} />,
+    onFilter: (value, record) => record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownVisibleChange: (visible) => {
+      if (visible) {
+        setTimeout(() => this.searchInput.select());
+      }
+    },
+    render: (text) => (
+      <Highlighter
+        highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+        searchWords={[this.state.searchText]}
+        autoEscape
+        textToHighlight={text.toString()}
+      />
+    ),
+  })
+
+  handleSearch = (selectedKeys, confirm) => {
+    confirm();
+    this.setState({ searchText: selectedKeys[0] });
+  }
+
+  handleReset = (clearFilters) => {
+    clearFilters();
+    this.setState({ searchText: '' });
+  }
+
   render() {
+    var permissionsFilter = this.state.permissions.map((p) => {return {text: p, value: p}});
+  
     return (
       <Table dataSource={this.state.groups}>
         <Column
           title="ID"
           dataIndex="id"
           key="id"
+          sorter={(a, b) => a.id - b.id }
+          {...this.getColumnSearchProps('id')}
         />
         <Column
           title="Name"
           dataIndex="name"
           key="name"
+          sorter={(a, b) => (a.name < b.name ? -1 : 1)}
+          {...this.getColumnSearchProps('name')}
         />
         <Column
           title="Permissions"
           dataIndex="permissions"
           key="permissions"
           render={permissions => this.renderPermissions(permissions)}
+          filters={permissionsFilter}
+          onFilter={(value, record) => record.permissions.includes(value)}
         />
         <Column
           title="Actions"
--- app/components/UsersTable.js
+++ app/components/UsersTable.js
@@ -1,22 +1,29 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import * as rpc from '../utils/rpc';
-import { Table, Popover, Tag, Button, message } from 'antd';
+import { Table, Popover, Tag, Button, Input, Icon, message } from 'antd';
+import Highlighter from 'react-highlight-words';
 
 const { Column } = Table;
 
 export default class UsersTable extends Component {
   state = {
-    users: []
+    users: [],
+    groups: [] // Used for filtering
   }
 
   async fetchUsers() {
     var users = await rpc.client.call('admin:getUsers');
+    var groups = [];
     users.forEach((user) => {
       user.key = user.id;
+      user.groups.forEach(group => {
+        if (!groups.includes(group))
+          groups.push(group);
+      });
     });
     console.log(users);
-    this.setState({users})
+    this.setState({users, groups})
   }
 
   async componentDidMount() {
@@ -39,18 +46,82 @@ export default class UsersTable extends Component {
       this.fetchUsers();
   }
 
+  getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({
+      setSelectedKeys, selectedKeys, confirm, clearFilters,
+    }) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          ref={node => { this.searchInput = node; }}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => this.handleSearch(selectedKeys, confirm)}
+          style={{ width: 188, marginBottom: 8, display: 'block' }}
+        />
+        <Button
+          type="primary"
+          onClick={() => this.handleSearch(selectedKeys, confirm)}
+          icon="search"
+          size="small"
+          style={{ width: 90, marginRight: 8 }}
+        >
+          Search
+        </Button>
+        <Button
+          onClick={() => this.handleReset(clearFilters)}
+          size="small"
+          style={{ width: 90 }}
+        >
+          Reset
+        </Button>
+      </div>
+    ),
+    filterIcon: filtered => <Icon type="search" style={{ color: filtered ? '#1890ff' : undefined }} />,
+    onFilter: (value, record) => record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownVisibleChange: (visible) => {
+      if (visible) {
+        setTimeout(() => this.searchInput.select());
+      }
+    },
+    render: (text) => (
+      <Highlighter
+        highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+        searchWords={[this.state.searchText]}
+        autoEscape
+        textToHighlight={text.toString()}
+      />
+    ),
+  })
+
+  handleSearch = (selectedKeys, confirm) => {
+    confirm();
+    this.setState({ searchText: selectedKeys[0] });
+  }
+
+  handleReset = (clearFilters) => {
+    clearFilters();
+    this.setState({ searchText: '' });
+  }
+
   render() {
+    var groupsFilter = this.state.groups.map((group) => {return {text: group, value: group}});
+  
     return (
       <Table dataSource={this.state.users} size="middle">
         <Column
           title="ID"
           dataIndex="id"
           key="id"
+          sorter={(a, b) => a.id - b.id}
+          {...this.getColumnSearchProps('id')}
         />
         <Column
           title="Name"
           dataIndex="name"
           key="name"
+          sorter={(a, b) => (a.name < b.name ? -1 : 1)}
+          {...this.getColumnSearchProps('name')}
         />
         <Column
           title="Tags"
@@ -61,6 +132,7 @@ export default class UsersTable extends Component {
               {tags.length}
             </Popover>
           )}
+          sorter={(a, b) => a.id - b.id}
         />
         <Column
           title="Groups"
@@ -69,6 +141,9 @@ export default class UsersTable extends Component {
           render={groups => (
             <span>{groups.map(group => <Tag key={group}>{group}</Tag>)}</span>
           )}
+          sorter={(a, b) => a.groups.length - b.groups.length}
+          filters={groupsFilter}
+          onFilter={(value, record) => record.groups.includes(value)}
         />
         <Column
           title="Actions"
